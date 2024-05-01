370e2130bd526715f67a0689c03006f5e42869ec
01Sriharsha <harshavasu463@gmail.com>
2024-05-01 23:57:35 +0530

    feat: add custom methods

--- backend/src/main/java/dev/sriharsha/bookstore/backend/repository/TransactionHistoryRepository.java
+++ backend/src/main/java/dev/sriharsha/bookstore/backend/repository/TransactionHistoryRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Integer> {
     Page<TransactionHistory> findByUserId(Pageable pageable, Integer userId);
 
     Page<TransactionHistory> findByBookOwnerId(Pageable pageable, Integer ownerId);
+
+    Boolean existsByBookIdAndUserId(Integer bookId, Integer userId);
+
+    Optional<TransactionHistory> findByBookIdAndUserId(Integer bookId, Integer userId);
 }
