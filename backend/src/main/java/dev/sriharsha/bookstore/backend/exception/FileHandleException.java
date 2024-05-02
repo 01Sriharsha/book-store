82caf3c0807a612ce57d12b5ed3e2dda04d057b0
01Sriharsha <harshavasu463@gmail.com>
2024-05-02 23:47:16 +0530

    feat: add file handle exception

---
+++ backend/src/main/java/dev/sriharsha/bookstore/backend/exception/FileHandleException.java
@@ -0,0 +1,7 @@
+package dev.sriharsha.bookstore.backend.exception;
+
+public class FileHandleException extends RuntimeException {
+    public FileHandleException(String message) {
+        super(message);
+    }
+}
