8fed133c0b37c94773bcc13d8e96af8db97442af
01Sriharsha <harshavasu463@gmail.com>
2024-05-03 23:41:26 +0530

    refactor: handle data integrity exception

--- backend/src/main/java/dev/sriharsha/bookstore/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/dev/sriharsha/bookstore/backend/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package dev.sriharsha.bookstore.backend.exception;
 import dev.sriharsha.bookstore.backend.dto.Response;
 import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.util.Arrays;
+
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
@@ -19,7 +22,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(
             Exception exception) {
-        log.error("Global Exception: {}", exception.getMessage());
+        log.error("Global Exception Message: {}", exception.getMessage());
+        System.out.println(Arrays.toString(exception.getStackTrace()));
         Response errorResponse = Response.builder().message(exception.getMessage()).build();
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -79,5 +83,12 @@ public class GlobalExceptionHandler {
             FileHandleException exception) {
         Response errorResponse = Response.builder().message(exception.getMessage()).build();
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDataIntegrityViolationException(
+            DataIntegrityViolationException exception) {
+        Response errorResponse = Response.builder().message("Invalid data").build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
