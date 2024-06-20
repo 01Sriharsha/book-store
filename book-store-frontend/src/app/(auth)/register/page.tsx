162be033b4b68bca66dff7a4833081ce4a3ebc06
01Sriharsha <harshavasu463@gmail.com>
2024-06-20 23:41:56 +0530

    feat: add register page

---
+++ book-store-frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RegisterSchema } from "@/lib/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function RegisterForm() {
+  const form = useForm<RegisterSchema>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      fullname: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      city: "",
+      dateOfBirth: "",
+    },
+  });
+
+  const onSubmit = (values: RegisterSchema) => {
+    console.log(values);
+  };
+  return (
+    <div className="h-screen grid place-items-center">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className=" bg-background border border-primary/30 p-4 rounded-xl w-full md:w-1/2"
+        >
+          <h1 className="text-2xl font-semibold text-primary text-center my-3">
+            Register
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <FormField
+              name="fullname"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Fullname</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="Enter fullname"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="Enter email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="dateOfBirth"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Date Of Birth</FormLabel>
+                  <FormControl>
+                    <Input type="date" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="city"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>City</FormLabel>
+                  <FormControl>
+                    <Input type="city" placeholder="Enter city" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      placeholder="Confirm Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="mt-5 mb-3 w-full flex items-center justify-center gap-4">
+            <Link href="/">
+              <Button type="button" variant={"ghost"}>
+                Back
+              </Button>
+            </Link>
+            <Button type="submit">Register</Button>
+          </div>
+          <div className="text-center mb-2 text-sm">
+            Already have an account?&nbsp;
+            <Link href="/?login=true" className="underline text-primary">
+              Register Now!
+            </Link>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
--- book-store-frontend/src/lib/zod.ts
+++ book-store-frontend/src/lib/zod.ts
@@ -5,4 +5,19 @@ export const LoginSchema = z.object({
   password: z.string().min(5, "Password must be at least 5 characters"),
 });
 
+export const RegisterSchema = z
+  .object({
+    fullname: z.string().min(1, { message: "Fullname is required" }),
+    email: z.string().email("Invalid email"),
+    city: z.string().min(1, { message: "City is required" }),
+    dateOfBirth: z.string().min(1, { message: "Date of birth is required" }),
+    password: z.string().min(5, "Password must be at least 5 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"], // specify the path to show the error
+  });
+
 export type LoginSchema = z.infer<typeof LoginSchema>;
+export type RegisterSchema = z.infer<typeof RegisterSchema>;
