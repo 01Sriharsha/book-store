d85d83674490d85942582c7b2bb196c8975cf22a
01Sriharsha <harshavasu463@gmail.com>
2024-06-25 23:52:41 +0530

    style: add background gradient

--- book-store-frontend/src/app/layout.tsx
+++ book-store-frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 import AppProvider from "@/providers";
 
 const roboto = Roboto({ subsets: ["latin"], weight: "400" });
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
+      <body className={cn(roboto.className , "bg-gradient-to-r from-zinc-800 to-zinc-950")}>
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
